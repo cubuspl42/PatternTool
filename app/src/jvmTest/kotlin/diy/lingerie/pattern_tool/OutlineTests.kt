dd889915e9366a5f09b9042d32e4c1e9ce6e9b28
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-04-27 18:11:11 +0200

    Add FOP dependency

--- app/src/jvmTest/kotlin/diy/lingerie/pattern_tool/OutlineTests.kt
+++ app/src/jvmTest/kotlin/diy/lingerie/pattern_tool/OutlineTests.kt
@@ -8,14 +8,15 @@ import diy.lingerie.test_utils.getResourceAsReader
 import diy.lingerie.utils.xml.svg.parseSvgDocument
 import diy.lingerie.utils.xml.writeToFile
 import org.apache.batik.anim.dom.SAXSVGDocumentFactory
+import org.apache.batik.parser.PathHandler
 import kotlin.io.path.Path
 import kotlin.test.Test
 
 val svgDocumentFactory = SAXSVGDocumentFactory(null)
 
-class OutlineSvgTests {
+class OutlineTests {
     @Test
-    fun testLoad() {
+    fun testLoadSvg() {
         val reader = ClosedSplineSvgTests::class.java.getResourceAsReader("closedPath1.svg")!!
         val svgDocument = svgDocumentFactory.parseSvgDocument(reader = reader)
 
@@ -190,6 +191,93 @@ class OutlineSvgTests {
 
         svgDocument.writeToFile(
             Path("../output/outlineDumpSvg1.svg")
+        )
+    }
+
+    @Test
+    fun testDumpPdf() {
+        val foReader = OutlineTests::class.java.getResourceAsReader("a4.fo")!!
+
+        val edgeMetadata = Outline.EdgeMetadata(
+            seamAllowance = SeamAllowance(allowanceMm = 6.0),
+        )
+
+        val outline = Outline(
+            links = listOf(
+                Outline.Link(
+                    startAnchor = Outline.Joint.Anchor(
+                        position = Point(32.551998138427734, 125.20800018310547),
+                    ),
+                    edge = Outline.Edge(
+                        startHandle = Outline.Joint.Handle(
+                            position = Point(43.84199905395508, 108.19000244140625),
+                        ),
+                        intermediateJoints = emptyList(),
+                        endHandle = Outline.Joint.Handle(
+                            position = Point(50.24800109863281, 84.95600128173828),
+                        ),
+                        metadata = edgeMetadata,
+                    ),
+                ),
+                Outline.Link(
+                    startAnchor = Outline.Joint.Anchor(
+                        position = Point(65.63800048828125, 72.54399871826172),
+                    ),
+                    edge = Outline.Edge(
+                        startHandle = Outline.Joint.Handle(
+                            position = Point(82.27100372314453, 59.12900161743164),
+                        ),
+                        intermediateJoints = emptyList(),
+                        endHandle = Outline.Joint.Handle(
+                            position = Point(108.29000091552734, 56.98699951171875),
+                        ),
+                        metadata = edgeMetadata,
+                    ),
+                ),
+                Outline.Link(
+                    startAnchor = Outline.Joint.Anchor(
+                        position = Point(131.3769989013672, 50.821998596191406),
+                    ),
+                    edge = Outline.Edge(
+                        startHandle = Outline.Joint.Handle(
+                            position = Point(126.60900115966797, 85.4229965209961),
+                        ),
+                        intermediateJoints = emptyList(),
+                        endHandle = Outline.Joint.Handle(
+                            position = Point(146.66000366210938, 103.48999786376953),
+                        ),
+                        metadata = edgeMetadata,
+                    ),
+                ),
+                Outline.Link(
+                    startAnchor = Outline.Joint.Anchor(
+                        position = Point(181.7220001220703, 111.55500030517578),
+                    ),
+                    edge = Outline.Edge(
+                        startHandle = null, intermediateJoints = emptyList(), endHandle = null, metadata = edgeMetadata
+                    ),
+                ),
+                Outline.Link(
+                    startAnchor = Outline.Joint.Anchor(
+                        position = Point(131.70599365234375, 177.86399841308594),
+                    ),
+                    edge = Outline.Edge(
+                        startHandle = Outline.Joint.Handle(
+                            position = Point(85.72000122070312, 174.21600341796875),
+                        ),
+                        intermediateJoints = emptyList(),
+                        endHandle = Outline.Joint.Handle(
+                            position = Point(49.132999420166016, 160.46400451660156),
+                        ),
+                        metadata = edgeMetadata,
+                    ),
+                ),
+            ),
+        )
+
+        outline.dumpPdf(
+            foReader = foReader,
+            outputPath = Path("../output/a4.pdf"),
         )
     }
 }
