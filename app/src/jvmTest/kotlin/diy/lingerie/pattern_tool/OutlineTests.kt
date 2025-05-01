65f84d9618ce11945275eed32797addbc989e237
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-01 17:19:13 +0200

    Sketch `testLoadSvg_newLogic`

--- app/src/jvmTest/kotlin/diy/lingerie/pattern_tool/OutlineTests.kt
+++ app/src/jvmTest/kotlin/diy/lingerie/pattern_tool/OutlineTests.kt
@@ -2,6 +2,7 @@ package diy.lingerie.pattern_tool
 
 import diy.lingerie.geometry.LineSegment
 import diy.lingerie.geometry.Point
+import diy.lingerie.geometry.curves.OpenCurve
 import diy.lingerie.simple_dom.mm
 import diy.lingerie.simple_dom.svg.SvgPath
 import diy.lingerie.simple_dom.svg.SvgRoot
@@ -12,7 +13,7 @@ import kotlin.test.Test
 
 class OutlineTests {
     @Test
-    fun testLoadSvg() {
+    fun testLoadSvg_oldLogic() {
         val point0FreeJoint = Point(849.97, 1083.35)
         val point1Control = Point(923.62, 1116.05)
         val point2Control = Point(1015.23, 1213.44)
@@ -226,6 +227,217 @@ class OutlineTests {
                         ),
                         endAnchor = Outline.Anchor(
                             position = point0FreeJoint
+                        ),
+                    ),
+                ),
+            ),
+            actual = outline,
+        )
+    }
+
+    @Test
+    @Ignore
+    fun testLoadSvg_newLogic() {
+        val point0FreeJoint = Point(849.97, 1083.35)
+        val point1Control = Point(923.62, 1116.05)
+        val point2Control = Point(1015.23, 1213.44)
+        val point3SmoothJoint = Point(1061.95, 1336.57)
+        val point4Control = Point(1111.89, 1468.18)
+        val point5Control = Point(1143.21, 1618.69)
+
+        val point6FreeJoint = Point(1149.75, 1695.65)
+        val point7Control = Point(1228.37, 1673.57)
+        val point8Control = Point(1302.86, 1563.13)
+
+        val point9FreeJoint = Point(1506.80, 1553.93)
+        val point10Control = Point(1750.43, 1542.92)
+        val point11Control = Point(1680.53, 1442.38)
+        val point12SmoothJoint = Point(1877.12, 1435.89)
+        val point13Control = Point(2069.22, 1429.54)
+        val point14Control = Point(2160.31, 1476.10)
+        val point15SmoothJoint = Point(2305.43, 1341.55)
+        val point16Control = Point(2267.93, 1108.87)
+        val point17Control = Point(2203.85, 790.42)
+
+        val point18FreeJoint = Point(1838.79, 561.24)
+        val point19Control = Point(1478.44, 611.55)
+        val point20Control = Point(1447.10, 715.54)
+        val point21SmoothJoint = Point(1283.83, 758.94)
+        val point22Control = Point(1084.05, 812.07)
+        val point23Control = Point(997.57, 842.14)
+
+        val svgRoot = SvgRoot(
+            children = listOf(
+                SvgPath(
+                    stroke = SvgPath.Stroke.default,
+                    segments = listOf(
+                        SvgPath.Segment.MoveTo(
+                            targetPoint = point0FreeJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point1Control,
+                            controlPoint2 = point2Control,
+                            finalPoint = point3SmoothJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point4Control,
+                            controlPoint2 = point5Control,
+                            finalPoint = point6FreeJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point7Control,
+                            controlPoint2 = point8Control,
+                            finalPoint = point9FreeJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point10Control,
+                            controlPoint2 = point11Control,
+                            finalPoint = point12SmoothJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point13Control,
+                            controlPoint2 = point14Control,
+                            finalPoint = point15SmoothJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point16Control,
+                            controlPoint2 = point17Control,
+                            finalPoint = point18FreeJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point19Control,
+                            controlPoint2 = point20Control,
+                            finalPoint = point21SmoothJoint,
+                        ),
+                        SvgPath.Segment.CubicBezierCurveTo(
+                            controlPoint1 = point22Control,
+                            controlPoint2 = point23Control,
+                            finalPoint = point0FreeJoint,
+                        ),
+                        SvgPath.Segment.ClosePath,
+                    ),
+                ),
+            ),
+            width = 4000.mm,
+            height = 4000.mm,
+        )
+
+        val outline = Outline.loadSvg(
+            svgRoot = svgRoot,
+        )
+
+        val expectedEdgeMetadata = Outline.EdgeMetadata(
+            seamAllowance = SeamAllowance(allowanceMm = 6.0),
+        )
+
+        assertEqualsWithTolerance(
+            expected = Outline(
+                links = listOf(
+                    Outline.Link(
+                        edge = Outline.Edge(
+                            startHandle = Outline.Handle(
+                                position = point1Control,
+                            ),
+                            intermediateJoints = listOf(
+                                Outline.Joint.Smooth(
+                                    rearHandle = Outline.Handle(
+                                        position = point2Control,
+                                    ),
+                                    anchorCoord = OpenCurve.Coord(
+                                        t = 0.0, // TODO
+                                    ),
+                                    frontHandle = Outline.Handle(
+                                        position = point4Control,
+                                    ),
+                                ),
+                            ),
+                            endHandle = Outline.Handle(
+                                position = point5Control,
+                            ),
+                            metadata = expectedEdgeMetadata,
+                        ),
+                        endAnchor = Outline.Anchor(
+                            position = point6FreeJoint,
+                        ),
+                    ),
+                    Outline.Link(
+                        edge = Outline.Edge(
+                            startHandle = Outline.Handle(
+                                position = point7Control,
+                            ),
+                            intermediateJoints = emptyList(),
+                            endHandle = Outline.Handle(
+                                position = point8Control,
+                            ),
+                            metadata = expectedEdgeMetadata,
+                        ),
+                        endAnchor = Outline.Anchor(
+                            position = point9FreeJoint,
+                        ),
+                    ),
+                    Outline.Link(
+                        edge = Outline.Edge(
+                            startHandle = Outline.Handle(
+                                position = point10Control,
+                            ),
+                            intermediateJoints = listOf(
+                                Outline.Joint.Smooth(
+                                    rearHandle = Outline.Handle(
+                                        position = point11Control,
+                                    ),
+                                    anchorCoord = OpenCurve.Coord(
+                                        t = 0.0, // TODO
+                                    ),
+                                    frontHandle = Outline.Handle(
+                                        position = point13Control,
+                                    ),
+                                ),
+                                Outline.Joint.Smooth(
+                                    rearHandle = Outline.Handle(
+                                        position = point14Control,
+                                    ),
+                                    anchorCoord = OpenCurve.Coord(
+                                        t = 0.0, // TODO
+                                    ),
+                                    frontHandle = Outline.Handle(
+                                        position = point16Control,
+                                    ),
+                                ),
+                            ),
+                            endHandle = Outline.Handle(
+                                position = point17Control,
+                            ),
+                            metadata = expectedEdgeMetadata,
+                        ),
+                        endAnchor = Outline.Anchor(
+                            position = point18FreeJoint,
+                        ),
+                    ),
+                    Outline.Link(
+                        edge = Outline.Edge(
+                            startHandle = Outline.Handle(
+                                position = point19Control,
+                            ),
+                            intermediateJoints = listOf(
+                                Outline.Joint.Smooth(
+                                    rearHandle = Outline.Handle(
+                                        position = point20Control,
+                                    ),
+                                    anchorCoord = OpenCurve.Coord(
+                                        t = 0.0, // TODO
+                                    ),
+                                    frontHandle = Outline.Handle(
+                                        position = point22Control,
+                                    ),
+                                ),
+                            ),
+                            endHandle = Outline.Handle(
+                                position = point23Control,
+                            ),
+                            metadata = expectedEdgeMetadata,
+                        ),
+                        endAnchor = Outline.Anchor(
+                            position = point0FreeJoint,
                         ),
                     ),
                 ),
