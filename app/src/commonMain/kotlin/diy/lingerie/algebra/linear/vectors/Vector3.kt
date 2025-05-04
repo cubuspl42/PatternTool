1d5eb1a21716a04d404d62ebbcbc7e9a066f8c36
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-04 13:54:57 +0200

    Implement `Vector3`, `Vector4`

--- app/src/commonMain/kotlin/diy/lingerie/algebra/linear/vectors/Vector3.kt
+++ app/src/commonMain/kotlin/diy/lingerie/algebra/linear/vectors/Vector3.kt
@@ -5,85 +5,98 @@ import diy.lingerie.algebra.NumericObject.Tolerance
 import diy.lingerie.algebra.equalsWithTolerance
 import kotlin.math.sqrt
 
-data class Vector2(
+data class Vector3(
     val a0: Double,
     val a1: Double,
+    val a2: Double,
 ) : NumericObject {
     companion object {
         fun full(
             a: Double,
-        ): Vector2 = Vector2(
+        ): Vector3 = Vector3(
             a0 = a,
             a1 = a,
+            a2 = a,
         )
     }
 
     val magnitudeSquared: Double
-        get() = a0 * a0 + a1 * a1
+        get() = a0 * a0 + a1 * a1 + a2 * a2
 
     val magnitude: Double
         get() = sqrt(magnitudeSquared)
 
     fun isNormalized(): Boolean = magnitudeSquared.equalsWithTolerance(1.0)
 
-    fun normalize(): Vector2 = this / magnitude
+    fun normalize(): Vector3 = this / magnitude
 
-    operator fun unaryMinus(): Vector2 = Vector2(
+    operator fun unaryMinus(): Vector3 = Vector3(
         a0 = -a0,
         a1 = -a1,
+        a2 = -a2,
     )
 
     operator fun plus(
         scalar: Double,
-    ): Vector2 = Vector2(
+    ): Vector3 = Vector3(
         a0 = a0 + scalar,
         a1 = a1 + scalar,
+        a2 = a2 + scalar,
     )
 
     operator fun minus(
-        other: Vector2,
-    ): Vector2 = Vector2(
+        other: Vector3,
+    ): Vector3 = Vector3(
         a0 = a0 - other.a0,
         a1 = a1 - other.a1,
+        a2 = a2 - other.a2,
     )
 
     operator fun times(
         scalar: Double,
-    ): Vector2 = Vector2(
+    ): Vector3 = Vector3(
         a0 = a0 * scalar,
         a1 = a1 * scalar,
+        a2 = a2 * scalar,
     )
 
     operator fun div(
         scalar: Double,
-    ): Vector2 = Vector2(
+    ): Vector3 = Vector3(
         a0 = a0 / scalar,
         a1 = a1 / scalar,
+        a2 = a2 / scalar,
     )
 
-    fun normalizeOrNull(): Vector2? {
+    fun normalizeOrNull(): Vector3? {
         val normalized = normalize()
         return normalized.takeIf { it.isNormalized() }
     }
 
     fun dot(
-        other: Vector2,
-    ): Double = a0 * other.a0 + a1 * other.a1
+        other: Vector3,
+    ): Double = a0 * other.a0 + a1 * other.a1 + a2 * other.a2
 
     fun cross(
-        other: Vector2,
-    ): Double = a0 * other.a1 - a1 * other.a0
+        other: Vector3,
+    ): Vector3 = Vector3(
+        a0 = a1 * other.a2 - a2 * other.a1,
+        a1 = a2 * other.a0 - a0 * other.a2,
+        a2 = a0 * other.a1 - a1 * other.a0,
+    )
 
     fun toList(): List<Double> = listOf(
         a0,
         a1,
+        a2,
     )
 
     override fun equalsWithTolerance(
         other: NumericObject,
         tolerance: Tolerance,
     ): Boolean = when {
-        other !is Vector2 -> false
+        other !is Vector3 -> false
+
         !a0.equalsWithTolerance(
             other = other.a0,
             tolerance = tolerance,
@@ -94,10 +107,15 @@ data class Vector2(
             tolerance = tolerance,
         ) -> false
 
+        !a2.equalsWithTolerance(
+            other = other.a2,
+            tolerance = tolerance,
+        ) -> false
+
         else -> true
     }
 }
 
 operator fun Double.times(
-    vector: Vector2,
-): Vector2 = vector * this
+    vector: Vector3,
+): Vector3 = vector * this
