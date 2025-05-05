186a1f2b956627a4a223691e95a1f506a76443c0
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-05 11:43:31 +0200

    Change `ParametricLineFunction.locatePoint` implementation

--- app/src/commonMain/kotlin/diy/lingerie/math/geometry/parametric_curve_functions/ParametricLineFunction.kt
+++ app/src/commonMain/kotlin/diy/lingerie/math/geometry/parametric_curve_functions/ParametricLineFunction.kt
@@ -3,9 +3,12 @@ package diy.lingerie.math.geometry.parametric_curve_functions
 import diy.lingerie.geometry.x
 import diy.lingerie.geometry.y
 import diy.lingerie.math.algebra.NumericObject
+import diy.lingerie.math.algebra.divideWithTolerance
+import diy.lingerie.math.algebra.equalsWithTolerance
 import diy.lingerie.math.algebra.linear.vectors.Vector2
 import diy.lingerie.math.geometry.ParametricPolynomial
 import diy.lingerie.math.geometry.implicit_curve_functions.ImplicitLineFunction
+import diy.lingerie.utils.avgOf
 
 /**
  * Represents a line in 2D space in parametric form: p = s + d * t
@@ -47,9 +50,26 @@ data class ParametricLineFunction(
     override fun locatePoint(
         point: Vector2,
         tolerance: NumericObject.Tolerance,
-    ): Double? = when {
-        d.x != 0.0 -> (point.x - s.x) / d.x
-        d.y != 0.0 -> (point.y - s.y) / d.y
-        else -> null
+    ): Double? {
+        val tx: Double? = (point.x - s.x).divideWithTolerance(
+            d.x,
+            tolerance = tolerance,
+        )
+
+        val ty: Double? = (point.y - s.y).divideWithTolerance(
+            d.y,
+            tolerance = tolerance,
+        )
+
+        return when {
+            tx != null && ty != null -> {
+                // If the point we locate is close tho the curve, p(avg(t_x, t_y))
+                // is a point on the curve that is closer to the located point
+                // than either p(t_x) or p(t_y)
+                avgOf(tx, ty)
+            }
+
+            else -> tx ?: ty
+        }
     }
 }
