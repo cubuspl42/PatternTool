1ab2e085ef459911e0eb398ac3ae357ea53357d2
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-05-30 13:54:25 +0200

    Fix mouse event names

--- app/src/jsMain/kotlin/diy/lingerie/web_tool/main.kt
+++ app/src/jsMain/kotlin/diy/lingerie/web_tool/main.kt
@@ -2,19 +2,40 @@ package diy.lingerie.web_tool
 
 import dev.toolkt.dom.pure.PureColor
 import dev.toolkt.dom.pure.PureUnit
+import dev.toolkt.dom.pure.percent
 import dev.toolkt.dom.pure.px
 import dev.toolkt.dom.pure.style.PureFlexAlignItems
 import dev.toolkt.dom.pure.style.PureFlexDirection
 import dev.toolkt.dom.pure.style.PureFlexJustifyContent
 import dev.toolkt.dom.reactive.style.ReactiveFlexStyle
 import dev.toolkt.dom.reactive.style.ReactiveStyle
+import dev.toolkt.dom.reactive.utils.createReactiveTextNode
 import dev.toolkt.dom.reactive.utils.html.createReactiveHtmlDivElement
+import dev.toolkt.dom.reactive.utils.html.getMouseMoveEventStream
+import dev.toolkt.dom.reactive.utils.html.getMouseOffsetCell
+import dev.toolkt.dom.reactive.utils.svg.createReactiveSvgCircleElement
+import dev.toolkt.dom.reactive.utils.svg.createReactiveSvgSvgElement
+import dev.toolkt.geometry.Point
 import dev.toolkt.reactive.cell.Cell
 import dev.toolkt.reactive.reactive_list.ReactiveList
 import kotlinx.browser.document
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.svg.SVGSVGElement
 
 fun main() {
-    val root = document.createReactiveHtmlDivElement(
+    val rootElement = createRootElement()
+
+    document.body!!.appendChild(rootElement)
+}
+
+private fun createRootElement(): HTMLDivElement {
+    val primaryViewport = createPrimaryViewport()
+
+    primaryViewport.getMouseMoveEventStream().listen {
+        println(it)
+    }
+
+    return document.createReactiveHtmlDivElement(
         style = ReactiveStyle(
             displayStyle = Cell.of(
                 ReactiveFlexStyle(
@@ -27,24 +48,50 @@ fun main() {
             backgroundColor = Cell.of(PureColor.lightGray),
         ),
         children = ReactiveList.of(
-            document.createReactiveHtmlDivElement(
-                style = ReactiveStyle(
-                    displayStyle = Cell.of(
-                        ReactiveFlexStyle(
-                            alignItems = PureFlexAlignItems.Center,
-                            justifyContent = PureFlexJustifyContent.Start,
-                        ),
-                    ),
-                    width = Cell.of(PureUnit.Percent.full),
-                    height = Cell.of(24.px),
-                    backgroundColor = Cell.of(PureColor.lightGray),
-                ),
-                children = ReactiveList.of(
-                    document.createTextNode("Hello"),
-                ),
-            )
+            createTopBar(
+                point = primaryViewport.getMouseOffsetCell(),
+            ),
+            primaryViewport,
+        ),
+    )
+}
+
+private fun createTopBar(
+    point: Cell<Point?>,
+): HTMLDivElement = document.createReactiveHtmlDivElement(
+    style = ReactiveStyle(
+        displayStyle = Cell.of(
+            ReactiveFlexStyle(
+                alignItems = PureFlexAlignItems.Center,
+                justifyContent = PureFlexJustifyContent.Start,
+            ),
+        ),
+        width = Cell.of(PureUnit.Percent.full),
+        height = Cell.of(24.px),
+        backgroundColor = Cell.of(PureColor.lightGray),
+    ),
+    children = ReactiveList.of(
+        document.createReactiveTextNode(
+            data = point.map { it.toString() },
+        ),
+    ),
+)
+
+private fun createPrimaryViewport(): SVGSVGElement = ReactiveList.looped { childrenLooped ->
+    val svgElement = document.createReactiveSvgSvgElement(
+        style = ReactiveStyle(
+            width = Cell.of(100.percent),
+            height = Cell.of(100.percent),
+        ),
+        children = childrenLooped,
+    )
+
+    val children = ReactiveList.of(
+        document.createReactiveSvgCircleElement(
+            position = Cell.of(Point(20.0, 20.0)),
+            radius = 4.0,
         ),
     )
 
-    document.body!!.appendChild(root)
+    return@looped Pair(svgElement, children)
 }
