7c031d81d753670afd8f4551c76f64a78ef7d5f0
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-06-25 12:08:01 +0200

    Improve `PlatformSystem` utils

--- DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/platform/PlatformSystem.kt
+++ DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/platform/PlatformSystem.kt
@@ -1,7 +1,11 @@
 package dev.toolkt.core.platform
 
 expect object PlatformSystem {
+    /**
+     * Collects garbage in a best-effort manner. This utility is meant to help with testing and debugging memory
+     * management issues, especially involved with weak references and memory leaks.
+     */
     fun collectGarbage()
 
-    fun log(value: Any)
+    fun log(value: Any?)
 }
--- DevToolkt/core/src/jsMain/kotlin/dev/toolkt/core/platform/PlatformSystem.kt
+++ DevToolkt/core/src/jsMain/kotlin/dev/toolkt/core/platform/PlatformSystem.kt
@@ -1,10 +1,23 @@
 package dev.toolkt.core.platform
 
 actual object PlatformSystem {
+    private var garbage: Any? = null
+
     actual fun collectGarbage() {
+        garbage = DummyGarbage.build(size = 1024 * 1024) // 1 MiB of garbage
     }
 
-    actual fun log(value: Any) {
+    actual fun log(value: Any?) {
         console.log(value)
+    }
+}
+
+private data class DummyGarbage(
+    val number: Int = 1,
+) {
+    companion object {
+        fun build(
+            size: Int,
+        ): Array<DummyGarbage> = Array(size) { DummyGarbage() }
     }
 }
