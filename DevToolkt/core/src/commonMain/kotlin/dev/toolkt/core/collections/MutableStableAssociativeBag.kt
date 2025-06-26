4e17e24f1d81e462fa206e642d699baecf4dde90
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-06-26 13:32:55 +0200

    Implement `StableBagBackedMutableStableAssociativeBag`

--- DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/collections/MutableStableAssociativeBag.kt
+++ DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/collections/MutableStableAssociativeBag.kt
@@ -1,3 +1,27 @@
 package dev.toolkt.core.collections
 
-interface MutableStableAssociativeBag<K, V> : MutableStableBag<Map.Entry<K, V>>, MutableAssociativeBag<K, V>
+interface MutableStableAssociativeBag<K, V> : MutableStableBag<Map.Entry<K, V>>, MutableAssociativeBag<K, V> {
+    companion object {
+        fun <K, V : Any> create(): MutableStableAssociativeBag<K, V> = backed(
+            // TODO: Can be replaced with a linked list
+            entries = MutableTreeList(),
+        )
+
+        fun <K, V : Any> backed(
+            entries: MutableStableBag<Map.Entry<K, V>>,
+        ): MutableStableAssociativeBag<K, V> = StableBagBackedStableAssociativeBag(
+            entries = entries,
+        )
+    }
+}
+
+private class StableBagBackedStableAssociativeBag<K, V : Any>(
+    private val entries: MutableStableBag<Map.Entry<K, V>>,
+) : MutableStableAssociativeBag<K, V>, MutableStableBag<Map.Entry<K, V>> by entries {
+    override fun getAll(key: K): Collection<V> = entries.mapNotNull {
+        when {
+            it.key == key -> it.value
+            else -> null
+        }
+    }
+}
