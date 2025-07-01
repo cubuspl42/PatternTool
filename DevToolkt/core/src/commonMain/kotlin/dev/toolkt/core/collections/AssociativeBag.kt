8faf94f82d27d15131da86f7cc6d59fa97cacc0e
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-01 11:23:49 +0200

    Nuke `AssociativeBag`
    ...as it doesn't seem to be semantically distinct from `MutliValuedMap`

--- DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/collections/AssociativeBag.kt
+++
@@ -1,7 +0,0 @@
-package dev.toolkt.core.collections
-
-/**
- * A collection associating a set of keys with a set of values in a one-to-many relation,
- * without focusing on lookup optimization.
- */
-interface AssociativeBag<K, out V> : Bag<Map.Entry<K, V>>, AssociativeCollection<K, V>
--- DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/collections/MutableAssociativeBag.kt
+++
@@ -1,4 +0,0 @@
-package dev.toolkt.core.collections
-
-interface MutableAssociativeBag<K, V> : MutableBag<Map.Entry<K, V>>, AssociativeBag<K, V>,
-    MutableAssociativeCollection<K, V>
