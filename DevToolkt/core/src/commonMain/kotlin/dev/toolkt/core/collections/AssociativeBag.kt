52860a27a66a9b35028f7b577d51247c255b49d1
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-01 11:04:35 +0200

    `AssociativeCollection`: Update comments

--- DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/collections/AssociativeBag.kt
+++ DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/collections/AssociativeBag.kt
@@ -1,6 +1,7 @@
 package dev.toolkt.core.collections
 
 /**
- * A collection associating a set of keys with a set of values in a many-to-many relation.
+ * A collection associating a set of keys with a set of values in a one-to-many relation,
+ * without focusing on lookup optimization.
  */
 interface AssociativeBag<K, out V> : Bag<Map.Entry<K, V>>, AssociativeCollection<K, V>
