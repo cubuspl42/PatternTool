2cba91ba914c1d930d0c0f309e59351756eac51c
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-04 20:06:40 +0200

    Generate more garbage
    ...during GC tests. 1M wasn't enough sometimes.

--- DevToolkt/core/src/jsMain/kotlin/dev/toolkt/core/platform/PlatformSystem.kt
+++ DevToolkt/core/src/jsMain/kotlin/dev/toolkt/core/platform/PlatformSystem.kt
@@ -4,7 +4,8 @@ actual object PlatformSystem {
     private var garbage: Any? = null
 
     actual fun collectGarbage() {
-        garbage = DummyGarbage.build(size = 1024 * 1024) // 1 MiB of garbage
+        // ~1M of garbage objects wasn't enough sometimes on the JS target, let's use a bit more
+        garbage = DummyGarbage.build(size = 4_000_000)
     }
 
     actual fun log(value: Any?) {
