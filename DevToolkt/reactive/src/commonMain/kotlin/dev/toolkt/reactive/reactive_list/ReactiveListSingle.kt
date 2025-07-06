5fee380f068b952842b571d0cbb51df959fc29f4
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-06 19:43:26 +0200

    Implement `ReactiveListSingle`
    ...in favor of `ReactiveListSingleOperator`

---
+++ DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/reactive_list/ReactiveListSingle.kt
@@ -0,0 +1,20 @@
+package dev.toolkt.reactive.reactive_list
+
+import dev.toolkt.reactive.cell.Cell
+import dev.toolkt.reactive.event_stream.EventStream
+
+class ReactiveListSingle<ElementT>(
+    private val elementCell: Cell<ElementT>,
+) : ActiveReactiveList<ElementT>() {
+    override val changes: EventStream<Change<ElementT>> = elementCell.newValues.mapNotNull { newValue ->
+        val update = Change.Update.set(
+            index = 0,
+            newValue = newValue,
+        )
+
+        Change.single(update = update)
+    }
+
+    override val currentElements: List<ElementT>
+        get() = listOf(elementCell.currentValue)
+}
