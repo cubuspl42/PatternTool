19440b79b2ebd30f8a3eb60a84d11f51ffddd4fe
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-06 11:37:32 +0200

    Sketch `PropertyCell`

---
+++ DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/cell/PropertyCell.kt
@@ -0,0 +1,75 @@
+package dev.toolkt.reactive.cell
+
+import dev.toolkt.reactive.Listener
+import dev.toolkt.reactive.event_stream.EventStream
+import dev.toolkt.reactive.future.Future
+
+class PropertyCell<ValueT : Any>(
+    initialValue: ValueT,
+) : ProperCell<ValueT>() {
+    sealed class State<out ValueT : Any> {
+        class Unbound<ValueT : Any>(
+            initialValue: ValueT,
+        ) : State<ValueT>() {
+            private val mutableValue = MutableCell(initialValue = initialValue)
+
+            override val exposedValue: Cell<ValueT>
+                get() = mutableValue
+
+            fun set(newValue: ValueT) {
+                mutableValue.set(newValue = newValue)
+            }
+        }
+
+        class Bound<ValueT : Any>(
+            private val boundValue: Cell<ValueT>,
+        ) : State<ValueT>() {
+            override val exposedValue: Cell<ValueT>
+                get() = boundValue
+        }
+
+        abstract val exposedValue: Cell<ValueT>
+    }
+
+    private val mutableState = MutableCell<State<ValueT>>(
+        State.Unbound(initialValue = initialValue),
+    )
+
+    val state: Cell<State<ValueT>>
+        get() = mutableState
+
+    val exposedValue = state.switchOf { it.exposedValue }
+
+    fun bindUntil(
+        boundValue: Cell<ValueT>,
+        until: Future<Unit>,
+    ) {
+        if (state.currentValue is State.Bound<*>) {
+            throw IllegalStateException("The property is already bound")
+        }
+
+        val newBoundState = State.Bound(
+            boundValue = boundValue,
+        )
+
+        until.onFulfilled.listen(
+            listener = object : Listener<Any?> {
+                override fun handle(event: Any?) {
+                    val finalUnboundState = State.Unbound(
+                        initialValue = newBoundState.exposedValue.currentValue,
+                    )
+
+                    mutableState.set(finalUnboundState)
+                }
+            },
+        )
+
+        mutableState.set(newBoundState)
+    }
+
+    override val newValues: EventStream<ValueT>
+        get() = exposedValue.newValues
+
+    override val currentValue: ValueT
+        get() = exposedValue.currentValue
+}
