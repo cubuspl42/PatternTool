d9121d1b0bcb9fdea94448f5bf04a19a44a14241
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-06 20:13:31 +0200

    Introduce `elements` / `newElements` operators

--- DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/reactive_list/ActiveReactiveList.kt
+++ DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/reactive_list/ActiveReactiveList.kt
@@ -1,6 +1,25 @@
 package dev.toolkt.reactive.reactive_list
 
+import dev.toolkt.core.delegates.weakLazy
+import dev.toolkt.reactive.cell.Cell
+import dev.toolkt.reactive.event_stream.EventStream
+import dev.toolkt.reactive.event_stream.hold
+
 abstract class ActiveReactiveList<E>() : ReactiveList<E>() {
+    final override val newElements: EventStream<List<E>> by lazy {
+        changes.map { change ->
+            val oldElements = currentElements.toMutableList()
+
+            change.applyTo(oldElements)
+
+            oldElements.toList()
+        }
+    }
+
+    final override val elements: Cell<List<E>> by weakLazy {
+        newElements.hold(currentElements)
+    }
+
     final override fun <Er> map(
         behavior: Behavior,
         transform: (E) -> Er,
