29fb6c94b7d1fdcb998f1beaf617f2456aacda85
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-08 14:43:45 +0200

    Sketch `ReactiveList.diff`

---
+++ DevToolkt/reactive/src/commonTest/kotlin/dev/toolkt/reactive/reactive_list/ReactiveListDiffTests.kt
@@ -0,0 +1,59 @@
+package dev.toolkt.reactive.reactive_list
+
+import dev.toolkt.reactive.cell.MutableCell
+import dev.toolkt.reactive.test_utils.EventStreamVerifier
+import kotlin.test.Ignore
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@Ignore
+class ReactiveListDiffTests {
+    @Test
+    fun testDiff_initial() {
+        val mutableListCell = MutableCell(
+            initialValue = listOf(10, 20, 30),
+        )
+
+        val diffReactiveList = ReactiveList.diff(mutableListCell)
+
+        assertEquals(
+            expected = listOf(10, 20, 30),
+            actual = diffReactiveList.currentElements,
+        )
+    }
+
+    @Test
+    fun testDiff_newValue() {
+        val mutableListCell = MutableCell(
+            initialValue = listOf(10, 20, 30),
+        )
+
+        val diffReactiveList = ReactiveList.diff(mutableListCell)
+
+
+        val changesVerifier = EventStreamVerifier(
+            eventStream = diffReactiveList.changes,
+        )
+
+        mutableListCell.set(
+            listOf(11, 21, 31, 41),
+        )
+
+        assertEquals(
+            expected = listOf(11, 21, 31, 41),
+            actual = diffReactiveList.currentElements,
+        )
+
+        assertEquals(
+            expected = listOf(
+                ReactiveList.Change.single(
+                    update = ReactiveList.Change.Update.change(
+                        indexRange = 0..2,
+                        changedElements = listOf(11, 21, 31, 41),
+                    ),
+                ),
+            ),
+            actual = changesVerifier.removeReceivedEvents(),
+        )
+    }
+}
