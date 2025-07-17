15b2a748bd282a34071c84d3c1c133f1afe1e5b2
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-17 13:32:50 +0200

    Present the extended part of the curve

--- app/src/jsMain/kotlin/diy/lingerie/web_tool/ReactiveBezierCurve.kt
+++ app/src/jsMain/kotlin/diy/lingerie/web_tool/ReactiveBezierCurve.kt
@@ -1,14 +1,26 @@
 package diy.lingerie.web_tool
 
+import dev.toolkt.core.iterable.LinSpace
+import dev.toolkt.dom.pure.PureColor
+import dev.toolkt.dom.pure.style.PureFill
+import dev.toolkt.dom.pure.style.PurePointerEvents
+import dev.toolkt.dom.pure.style.PureStrokeStyle
 import dev.toolkt.dom.reactive.style.ReactiveStyle
 import dev.toolkt.dom.reactive.utils.svg.createReactiveSvgPathElement
+import dev.toolkt.dom.reactive.utils.svg.createReactiveSvgPolylineElement
 import dev.toolkt.geometry.Point
 import dev.toolkt.geometry.curves.BezierCurve
+import dev.toolkt.geometry.x
+import dev.toolkt.geometry.y
+import dev.toolkt.math.algebra.sample
 import dev.toolkt.reactive.cell.Cell
 import dev.toolkt.reactive.reactive_list.ReactiveList
 import kotlinx.browser.document
 import org.w3c.dom.extra.svg.SVGPathSegment
+import org.w3c.dom.svg.SVGElement
 import org.w3c.dom.svg.SVGPathElement
+import org.w3c.dom.svg.SVGSVGElement
+import svg.createLegacySVGPoint
 
 data class ReactiveBezierCurve(
     val start: Cell<Point>,
@@ -64,6 +76,37 @@ data class ReactiveBezierCurve(
             },
         ),
     )
+
+    fun createReactiveExtendedSvgPolylineElement(
+        svgElement: SVGSVGElement,
+    ): SVGElement {
+        val maxT = 5.0
+
+        return document.createReactiveSvgPolylineElement(
+            style = ReactiveStyle(
+                fill = Cell.of(PureFill.None),
+                strokeStyle = PureStrokeStyle(
+                    color = PureColor.darkGray,
+                ),
+                pointerEvents = Cell.of(PurePointerEvents.None),
+            ),
+            points = ReactiveList.diff(
+                bezierCurve.map { bezierCurveNow ->
+                    bezierCurveNow.basisFunction.sample(
+                        linSpace = LinSpace(
+                            range = -maxT..maxT,
+                            sampleCount = 1024,
+                        )
+                    ).map {
+                        svgElement.createLegacySVGPoint(
+                            x = it.b.x,
+                            y = it.b.y,
+                        )
+                    }
+                },
+            ),
+        )
+    }
 }
 
 private fun Point.toArray(): Array<Number> = arrayOf(x, y)
