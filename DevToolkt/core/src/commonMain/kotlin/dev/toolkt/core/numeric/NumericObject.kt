d961826afa4e05d0c714348f326389c968a747c8
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-07-21 15:08:50 +0200

    Refactor `Tolerance` hierarchy

--- DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/numeric/NumericObject.kt
+++ DevToolkt/core/src/commonMain/kotlin/dev/toolkt/core/numeric/NumericObject.kt
@@ -6,13 +6,37 @@ import kotlin.math.abs
 
 interface NumericObject {
     sealed class Tolerance {
+        /**
+         * Zero tolerance
+         *
+         * The only value that will be considered equal to the reference value
+         * is the reference value itself.
+         *
+         * Useful in cases where an algorithm was designed with numeric tolerance
+         * in mind, but in a specific case it's actually preferable to use exact
+         * equality.
+         */
         data object Zero : Tolerance() {
             override fun equalsApproximately(
-                value: Double, reference: Double,
+                value: Double,
+                reference: Double,
             ): Boolean = value == reference
         }
 
+        /**
+         * Threshold tolerance: |v - v_ref| ≤ t(v_ref)
+         */
         sealed class Threshold : Tolerance() {
+            companion object {
+                fun hybrid(
+                    relative: Relative,
+                    absolute: Absolute,
+                ): Hybrid = Hybrid(
+                    relative = relative,
+                    absolute = absolute,
+                )
+            }
+
             final override fun equalsApproximately(
                 value: Double,
                 reference: Double,
@@ -21,7 +45,45 @@ interface NumericObject {
             abstract fun threshold(reference: Double): Double
         }
 
+        /**
+         * Hybrid tolerance
+         *
+         * Hybrid tolerance uses the higher of two thresholds determined by
+         * the [relative] and the [absolute] tolerance.
+         *
+         * Useful for cases where the compared values are expected to be
+         * potentially both very small and very large, but it's also possible
+         * that the reference value can be exactly zero. In such cases, relative
+         * tolerance degrades to zero tolerance, which might be unacceptable.
+         */
+        data class Hybrid(
+            /**
+             * Relative tolerance, effective in the general case
+             */
+            val relative: Relative,
+            /**
+             * Absolute tolerance, effective for reference values close to zero
+             */
+            val absolute: Absolute,
+        ) : Threshold() {
+            override fun threshold(
+                reference: Double,
+            ): Double = maxOf(
+                relative.threshold(reference),
+                absolute.threshold(reference),
+            )
+        }
+
+        /**
+         * Absolute tolerance: |v - v_ref| ≤ a
+         *
+         * Useful for cases where the order of magnitude of the compared values
+         * is known up front.
+         */
         data class Absolute(
+            /**
+             * The absolute tolerance threshold
+             */
             val absoluteTolerance: Double,
         ) : Threshold() {
             operator fun times(factor: Double) = Absolute(
@@ -33,7 +95,19 @@ interface NumericObject {
             ): Double = absoluteTolerance
         }
 
+        /**
+         * Asymmetric relative tolerance: |v - v_ref| ≤ r * |v_ref|
+         *
+         * Useful for cases where the compared values are expected to be
+         * potentially both very small and very large. If the reference value
+         * is zero, it should be noted that _only_ ±0.0 will be considered
+         * equal to the reference value (within tolerance), no matter what
+         * relative tolerance factor we pick.
+         */
         data class Relative(
+            /**
+             * The relative tolerance factor
+             */
             val relativeTolerance: Double,
         ) : Threshold() {
             override fun threshold(
@@ -51,6 +125,10 @@ interface NumericObject {
             )
         }
 
+        /**
+         * Check if the [value] is approximately equal to the [reference] within
+         * the tolerance defined by this object. This operation might be asymmetric.
+         */
         abstract fun equalsApproximately(
             value: Double,
             reference: Double,
