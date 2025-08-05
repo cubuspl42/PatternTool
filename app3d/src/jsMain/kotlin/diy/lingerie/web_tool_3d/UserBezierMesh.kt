e2125f20501c6cd5cacc91543ae2b8af62caec86
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-05 17:38:55 +0200

    `UserBezierMesh`: Initialize properties in the class body

--- app3d/src/jsMain/kotlin/diy/lingerie/web_tool_3d/UserBezierMesh.kt
+++ app3d/src/jsMain/kotlin/diy/lingerie/web_tool_3d/UserBezierMesh.kt
@@ -1,30 +1,33 @@
 package diy.lingerie.web_tool_3d
 
-import dev.toolkt.geometry.Point
 import dev.toolkt.geometry.Point3D
 import dev.toolkt.geometry.curves.BezierCurve
 import dev.toolkt.reactive.cell.Cell
 import dev.toolkt.reactive.cell.PropertyCell
 
 class UserBezierMesh private constructor(
-    val apexPosition: PropertyCell<Point3D>,
-    val point0: PropertyCell<Point>,
-    val point1: PropertyCell<Point>,
-    val point2: PropertyCell<Point>,
-    val point3: PropertyCell<Point>,
+    initialApexVertex: Point3D,
+    initialBezierCurve: BezierCurve,
 ) {
     companion object {
         fun create(
             initialApexVertex: Point3D,
             initialBezierCurve: BezierCurve,
         ): UserBezierMesh = UserBezierMesh(
-            apexPosition = PropertyCell(initialValue = initialApexVertex),
-            point0 = PropertyCell(initialValue = initialBezierCurve.start),
-            point1 = PropertyCell(initialValue = initialBezierCurve.firstControl),
-            point2 = PropertyCell(initialValue = initialBezierCurve.secondControl),
-            point3 = PropertyCell(initialValue = initialBezierCurve.end),
+            initialApexVertex = initialApexVertex,
+            initialBezierCurve = initialBezierCurve,
         )
     }
+
+    val apexPosition = PropertyCell(initialValue = initialApexVertex)
+
+    val point0 = PropertyCell(initialValue = initialBezierCurve.firstControl)
+
+    val point1 = PropertyCell(initialValue = initialBezierCurve.secondControl)
+
+    val point2 = PropertyCell(initialValue = initialBezierCurve.end)
+
+    val point3 = PropertyCell(initialValue = initialBezierCurve.start)
 
     val bezierCurve: Cell<BezierCurve> = Cell.map4(
         point0,
