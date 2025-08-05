149f7c39cf88963022bd8f6ecbea3fbcab3b74c9
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-05 14:51:55 +0200

    Add support for `THREE.Vector3.project`

--- app3d/src/jsMain/kotlin/diy/lingerie/web_tool_3d/threeGeometryUtils.kt
+++ app3d/src/jsMain/kotlin/diy/lingerie/web_tool_3d/threeGeometryUtils.kt
@@ -2,9 +2,7 @@ package diy.lingerie.web_tool_3d
 
 import dev.toolkt.geometry.Point
 import dev.toolkt.geometry.Point3D
-import dev.toolkt.geometry.Ray
 import dev.toolkt.geometry.Ray3
-import dev.toolkt.geometry.z
 import three.THREE
 import three.worldPosition
 
@@ -17,15 +15,25 @@ fun THREE.Camera.castRay(
     // A 3D NDC target point in the far direction
     val targetNdcPoint = ndcPoint.toPoint3D(z = 1.0)
 
-    val targetWorldPoint = targetNdcPoint.toThreeJsVector3().apply {
-        unproject(camera = this@castRay)
-    }.toPoint3D()
+    val targetWorldPoint = targetNdcPoint.unproject(camera = this@castRay)
 
     return Ray3.of(
         origin = this@castRay.worldPosition.toPoint3D(),
         target = targetWorldPoint,
     )
 }
+
+fun Point3D.project(
+    camera: THREE.Camera,
+): Point3D = toThreeJsVector3().apply {
+    project(camera = camera)
+}.toPoint3D()
+
+fun Point3D.unproject(
+    camera: THREE.Camera,
+): Point3D = toThreeJsVector3().apply {
+    unproject(camera = camera)
+}.toPoint3D()
 
 fun Point3D.toThreeJsVector3() = THREE.Vector3(
     x = this.x,
