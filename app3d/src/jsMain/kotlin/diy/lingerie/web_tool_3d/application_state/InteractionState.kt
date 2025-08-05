70b76a7dff02c6c752c1cef5c712762c8cebdfe4
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-05 17:19:45 +0200

    Re-enable `ongoingHandleDragInteraction` code

--- app3d/src/jsMain/kotlin/diy/lingerie/web_tool_3d/application_state/InteractionState.kt
+++ app3d/src/jsMain/kotlin/diy/lingerie/web_tool_3d/application_state/InteractionState.kt
@@ -2,6 +2,7 @@ package diy.lingerie.web_tool_3d.application_state
 
 import dev.toolkt.geometry.Point
 import dev.toolkt.reactive.cell.Cell
+import dev.toolkt.reactive.cell.MutableCell
 import dev.toolkt.reactive.cell.PropertyCell
 import dev.toolkt.reactive.future.Future
 
@@ -9,37 +10,35 @@ class InteractionState(
     private val documentState: DocumentState,
 ) {
     class HandleDragInteraction(
-        /**
-         * A point which's movement determines the new handle position
-         */
         val requestedHandlePosition: Cell<Point>,
     )
 
-    // TODO: Re-enable this code
-//    private val ongoingHandleDragInteraction: MutableCell<HandleDragInteraction?> = MutableCell(
-//        initialValue = null,
-//    )
+    private val ongoingHandleDragInteraction: MutableCell<HandleDragInteraction?> = MutableCell(
+        initialValue = null,
+    )
 
     fun startHandleDragInteraction(
         handlePosition: PropertyCell<Point>,
         requestedHandlePosition: Cell<Point>,
         until: Future<Unit>,
-    ) {
-//        if (ongoingHandleDragInteraction.currentValue != null) return
+    ): HandleDragInteraction? {
+        if (ongoingHandleDragInteraction.currentValue != null) return null
 
-//        val newHandleDragInteraction = HandleDragInteraction(
-//            requestedHandlePosition = requestedHandlePosition,
-//        )
+        val newHandleDragInteraction = HandleDragInteraction(
+            requestedHandlePosition = requestedHandlePosition,
+        )
 
-//        ongoingHandleDragInteraction.set(newHandleDragInteraction)
+        ongoingHandleDragInteraction.set(newHandleDragInteraction)
 
-//        until.onFulfilled.forEach {
-//            ongoingHandleDragInteraction.set(null)
-//        }
+        until.onFulfilled.forEach {
+            ongoingHandleDragInteraction.set(null)
+        }
 
         handlePosition.bindUntil(
             boundValue = requestedHandlePosition,
             until = until,
         )
+
+        return newHandleDragInteraction
     }
 }
