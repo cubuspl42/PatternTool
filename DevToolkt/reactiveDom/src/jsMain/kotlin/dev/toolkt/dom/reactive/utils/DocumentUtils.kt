ded56549a3091587ea060d10a3b99fdfc931a6ad
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-13 12:58:42 +0200

    `createReactiveElement`: Clean up

--- DevToolkt/reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/DocumentUtils.kt
+++ DevToolkt/reactiveDom/src/jsMain/kotlin/dev/toolkt/dom/reactive/utils/DocumentUtils.kt
@@ -9,21 +9,16 @@ import org.w3c.dom.Element
 import org.w3c.dom.Node
 import org.w3c.dom.css.ElementCSSInlineStyle
 
+/**
+ * Creates a reactive element of type [ElementT] in the [Document].
+ *
+ * Binds to [ElementCSSInlineStyle.style] and [Node] child list.
+ */
 fun <ElementT : Element> Document.createReactiveElement(
     createElement: Document.() -> ElementT,
     style: ReactiveStyle? = null,
     children: ReactiveList<Node>? = null,
 ): ElementT = createElement().apply {
-    bind(
-        style = style,
-        children = children,
-    )
-}
-
-private fun Element.bind(
-    style: ReactiveStyle? = null,
-    children: ReactiveList<Node>? = null,
-) {
     @Suppress("UNCHECKED_CAST_TO_EXTERNAL_INTERFACE") (this as ElementCSSInlineStyle)
 
     style?.bind(
