885fb0b4d8ec22ae456a2298620ac527b4d974ec
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-13 12:34:49 +0200

    Define simple type-safe utils for creating HTML elements

--- DevToolkt/domApiCompatExtra/README.md
+++ DevToolkt/domApiCompatExtra/README.md
@@ -2,3 +2,5 @@
 
 Typings for DOM APIs that are a part of [HTML Living Standard](https://html.spec.whatwg.org/multipage/), but are not included
 in the [Kotlin DOM API compatibility library](https://mvnrepository.com/artifact/org.jetbrains.kotlin/kotlin-dom-api-compat).
+
+Also, some relatively unopinionated DOM utils.
---
+++ DevToolkt/domApiCompatExtra/src/jsMain/kotlin/org/w3c/dom/extra/documentUtils.kt
@@ -0,0 +1,15 @@
+package org.w3c.dom.extra
+
+import org.w3c.dom.Document
+import org.w3c.dom.HTMLButtonElement
+import org.w3c.dom.HTMLCanvasElement
+import org.w3c.dom.HTMLDivElement
+import org.w3c.dom.HTMLSpanElement
+
+fun Document.createCanvasElement(): HTMLCanvasElement = createElement("canvas") as HTMLCanvasElement
+
+fun Document.createDivElement(): HTMLDivElement = createElement("div") as HTMLDivElement
+
+fun Document.createButtonElement(): HTMLButtonElement = createElement("button") as HTMLButtonElement
+
+fun Document.createSpanElement(): HTMLSpanElement = createElement("span") as HTMLSpanElement
