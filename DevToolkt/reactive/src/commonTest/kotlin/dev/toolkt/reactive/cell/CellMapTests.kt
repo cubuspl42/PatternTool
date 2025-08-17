4064d289a0ba34a35013e6a6c8ec01a7b5eb0eb8
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-17 18:52:44 +0200

    Sketch `Cell.mapAt`

--- DevToolkt/reactive/src/commonTest/kotlin/dev/toolkt/reactive/cell/CellMapTests.kt
+++ DevToolkt/reactive/src/commonTest/kotlin/dev/toolkt/reactive/cell/CellMapTests.kt
@@ -3,57 +3,107 @@ package dev.toolkt.reactive.cell
 import dev.toolkt.reactive.test_utils.EventStreamVerifier
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class CellMapTests {
     @Test
-    fun testMap() {
-        val mutableCell = MutableCell(
+    fun testMap_listened() {
+        val mutableCell = MutableCell.createExternally(
             initialValue = 0,
         )
 
         val mappedCell = mutableCell.map { "$it" }
 
-        val changesVerifier = EventStreamVerifier(
-            eventStream = mappedCell.changes,
+        assertFalse(
+            actual = mutableCell.hasListeners,
         )
 
         assertEquals(
             expected = "0",
-            actual = mappedCell.currentValue,
+            actual = mappedCell.sampleExternally(),
         )
 
-        mutableCell.setUnmanaged(1)
+        val newValuesVerifier = EventStreamVerifier(
+            eventStream = mappedCell.newValues,
+        )
+
+        assertTrue(
+            actual = mutableCell.hasListeners,
+        )
+
+        mutableCell.setExternally(1)
+
+        assertEquals(
+            expected = listOf("1"),
+            actual = newValuesVerifier.removeReceivedEvents(),
+        )
 
         assertEquals(
             expected = "1",
-            actual = mappedCell.currentValue,
+            actual = mappedCell.sampleExternally(),
         )
+
+        mutableCell.setExternally(2)
 
         assertEquals(
-            expected = listOf(
-                Cell.Change(
-                    oldValue = "0",
-                    newValue = "1",
-                ),
-            ),
-            actual = changesVerifier.removeReceivedEvents(),
+            expected = listOf("2"),
+            actual = newValuesVerifier.removeReceivedEvents(),
         )
-
-        mutableCell.setUnmanaged(2)
 
         assertEquals(
             expected = "2",
-            actual = mappedCell.currentValue,
+            actual = mappedCell.sampleExternally(),
+        )
+    }
+
+    @Test
+    fun testMap_unlistened() {
+        val mutableCell = MutableCell.createExternally(
+            initialValue = 0,
+        )
+
+        val mappedCell = mutableCell.map { "$it" }
+
+        assertFalse(
+            actual = mutableCell.hasListeners,
         )
 
         assertEquals(
-            expected = listOf(
-                Cell.Change(
-                    oldValue = "1",
-                    newValue = "2",
-                ),
-            ),
-            actual = changesVerifier.removeReceivedEvents(),
+            expected = "0",
+            actual = mappedCell.sampleExternally(),
+        )
+
+        val newValuesVerifier = EventStreamVerifier(
+            eventStream = mappedCell.newValues,
+        )
+
+        assertTrue(
+            actual = mutableCell.hasListeners,
+        )
+
+        mutableCell.setExternally(1)
+
+        assertEquals(
+            expected = listOf("1"),
+            actual = newValuesVerifier.removeReceivedEvents(),
+        )
+
+        assertEquals(
+            expected = "1",
+            actual = mappedCell.sampleExternally(),
+        )
+
+        mutableCell.setExternally(2)
+
+        assertEquals(
+            expected = listOf("2"),
+            actual = newValuesVerifier.removeReceivedEvents(),
+        )
+
+        assertEquals(
+            expected = "2",
+            actual = mappedCell.sampleExternally(),
         )
     }
 }
