6467e145989ae47625a55c7ac39276fcec0a29bf
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-23 12:35:14 +0200

    Nuke `PassiveEventStream`

--- DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/cell/StatefulCell.kt
+++ DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/cell/StatefulCell.kt
@@ -2,11 +2,13 @@ package dev.toolkt.reactive.cell
 
 import dev.toolkt.core.platform.PlatformWeakReference
 import dev.toolkt.reactive.UnconditionalListener
-import dev.toolkt.reactive.event_stream.PassiveEventStream
 import dev.toolkt.reactive.event_stream.EventStream
 import dev.toolkt.reactive.event_stream.pinWeak
 import dev.toolkt.reactive.effect.MomentContext
 import dev.toolkt.reactive.effect.Transaction
+import dev.toolkt.reactive.event_stream.VertexEventStream
+import dev.toolkt.reactive.event_stream.vertex.EventStreamVertex
+import dev.toolkt.reactive.event_stream.vertex.PassiveEventStreamVertex
 
 /**
  * A cell with an inherent state, i.e. such that is not a pure function of its sources (that cannot be recomputed).
@@ -17,15 +19,15 @@ abstract class StatefulCell<V>(
 ) : ProperCell<V>() {
     private var storedValue: V = initialValue
 
-    final override val newValues: EventStream<V> = object : PassiveEventStream<V>() {
+    final override val newValues: EventStream<V> = object : VertexEventStream<V>() {
         // The `newValues` stream cannot store a strong reference to its outer cell, as that would keep it alive even
         // when maintaining state is not needed anymore (as no objects have a proper reference to that cell anymore).
         private val weakCell = PlatformWeakReference(this@StatefulCell)
 
-        private val self = this // Kotlin doesn't ofer a label for `this@PassiveEventStream` (why?)
+        override val vertex: EventStreamVertex<V> = object : PassiveEventStreamVertex<V>() {
+            private val self = this // Kotlin doesn't ofer a label for `this@PassiveEventStreamVertex` (why?)
 
-        override fun observe() = givenValues.listen(
-            listener = object : UnconditionalListener<V>() {
+            override fun observe() = givenValues.listen(listener = object : UnconditionalListener<V>() {
                 override fun handleUnconditionally(
                     transaction: Transaction,
                     event: V,
@@ -47,12 +49,12 @@ abstract class StatefulCell<V>(
 
                     return
                 }
-            }
-        )
+            })
 
-        init {
-            // Pin the event stream to keep the stored value up-to-date even when there are no listeners
-            self.pinWeak(target = this@StatefulCell)
+            init {
+                // Pin the event stream to keep the stored value up-to-date even when there are no listeners
+                self.pinWeak(target = this@StatefulCell)
+            }
         }
     }
 
