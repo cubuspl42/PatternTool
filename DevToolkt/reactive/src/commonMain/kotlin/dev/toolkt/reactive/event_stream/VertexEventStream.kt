aa592519c3cede5e818a6ae7d5f2b62d8af72f76
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-23 11:40:26 +0200

    Port `EventStreamSlot` to vertex

--- DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream/VertexEventStream.kt
+++ DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream/VertexEventStream.kt
@@ -8,12 +8,12 @@ import dev.toolkt.reactive.event_stream.vertex.subscribeNow
 /**
  * TODO: Remove duplication with [BasicEventStream]
  */
-internal abstract class VertexEventStream<EventT> : ProperEventStream<EventT>() {
+abstract class VertexEventStream<EventT> : ProperEventStream<EventT>() {
     final override fun listen(
         listener: Listener<EventT>,
     ): Subscription = vertex.subscribeNow(
         listener = listener,
     )
 
-    abstract val vertex: EventStreamVertex<EventT>
+    internal abstract val vertex: EventStreamVertex<EventT>
 }
