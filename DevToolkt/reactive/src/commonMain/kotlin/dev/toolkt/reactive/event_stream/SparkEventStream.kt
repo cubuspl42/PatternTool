7c531a865dbe9dc95ae59177ee82e5b6b64597ee
Jakub Trzebiatowski <cubuspl42@gmail.com>
2025-08-23 12:38:55 +0200

    Port `SparkEventStream` to vertices

--- DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream/SparkEventStream.kt
+++ DevToolkt/reactive/src/commonMain/kotlin/dev/toolkt/reactive/event_stream/SparkEventStream.kt
@@ -1,27 +1,20 @@
 package dev.toolkt.reactive.event_stream
 
 import dev.toolkt.reactive.effect.MomentContext
+import dev.toolkt.reactive.event_stream.vertex.EventStreamVertex
+import dev.toolkt.reactive.event_stream.vertex.SparkEventStreamVertex
 
-internal class SparkEventStream<EventT> private constructor() : BasicEventStream<EventT>() {
+internal class SparkEventStream<EventT> private constructor(
+    override val vertex: EventStreamVertex<EventT>,
+) : VertexEventStream<EventT>() {
     companion object {
         context(momentContext: MomentContext) fun <EventT> construct(
             event: EventT,
-        ): SparkEventStream<EventT> = SparkEventStream<EventT>().apply {
-            momentContext.transaction.enqueueFollowup {
-                notify(
-                    transaction = it,
-                    event = event,
-                )
-            }
-        }
-    }
-
-    override fun onResumed() {
-    }
-
-    override fun onPaused() {
-    }
-
-    override fun onAborted() {
+        ): SparkEventStream<EventT> = SparkEventStream(
+            vertex = SparkEventStreamVertex.construct(
+                transaction = momentContext.transaction,
+                event = event,
+            ),
+        )
     }
 }
